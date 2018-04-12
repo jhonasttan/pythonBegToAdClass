121ed53ee634aa871d7a6d71ed97b45f2cd597ad
jhonasttan <jhonasttan@gmail.com>
2018-04-11 20:31:02 -0400

    Sect: 8 - Project #3 - Web Scraper - 63. Parsing our soup
    Signed-off-by: jhonasttan <jhonasttan@gmail.com>

--- sec7-IntroductionToRequests/webscrapery/main.py
+++ sec7-IntroductionToRequests/webscrapery/main.py
@@ -1,10 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-bing = "http://www.bing.com/search"
-search = input("Enter search term: ")
+url = "http://www.bing.com/search"
+search = input("Search for: ")
 params = {"q": search}
-r = requests.get(bing, params=params)
+r = requests.get(url, params=params)
 
 soup = BeautifulSoup(r.text, "html.parser")
-print(soup.prettify())
+
+#print(soup.prettify())
+results = soup.find("ol", {"id": "b_results"})
+#print(results)
+
+links = results.findAll("li", {"class": "b_algo"})
+#print(links)
+
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
