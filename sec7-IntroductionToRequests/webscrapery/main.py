4a15706634a2e75a9b5b81214e5c93d231232fa4
jhonasttan <jhonasttan@gmail.com>
2018-04-12 20:18:41 -0400

    Sect: 8 - Project #3 - Web Scraper - 64. Directional navigation
    Signed-off-by: jhonasttan <jhonasttan@gmail.com>

--- sec7-IntroductionToRequests/webscrapery/main.py
+++ sec7-IntroductionToRequests/webscrapery/main.py
@@ -7,21 +7,18 @@ params = {"q": search}
 r = requests.get(url, params=params)
 
 soup = BeautifulSoup(r.text, "html.parser")
-
 results = soup.find("ol", {"id": "b_results"})
-links = results.findAll("li", {"class": "b_algo"}) #c_tlbxH b_algo
+links = results.findAll("li", {"class": "b_algo"})
 
 for item in links:
     item_text = item.find("a").text
     item_href = item.find("a").attrs["href"]
+    item_summ = item.find("p").text
 
     if item_text and item_href:
         print("\n", item_text)
         print(item_href)
-        #print("Summary:", item.find("a").parent.parent.find("a").text)
-        #children = item.children
-        #for child in children:
-        #    print("Child:", child)
-        #    print(child.find("a"))
+        print(item_summ)
+
         children = item.find("h2")
-        print("Next sibling of the h2:", children.next_sibling)
+        print("Next sibling of the h2:", children)
