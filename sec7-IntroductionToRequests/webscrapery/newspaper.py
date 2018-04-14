63f74c4fdc37ff0c2838c9788731a38046db3025
jhonasttan <jhonasttan@gmail.com>
2018-04-14 14:07:17 -0400

    Sect: 8 - Project #3 - Web Scraper - newspaper3k example in file newspaper.py
    Signed-off-by: jhonasttan <jhonasttan@gmail.com>

---
+++ sec7-IntroductionToRequests/webscrapery/newspaper.py
@@ -0,0 +1,46 @@
+'''
+    Author: Jhonasttan Regalado
+    Copyright: 20180414
+    Script: Quick use of Python library newspaper3k
+    requirements: https://pypi.python.org/pypi/newspaper3k/0.1.5
+        From the requirements site, below is a quick shortcut for OSX users (like me):
+            $ brew install libxml2 libxslt
+            $ brew install libtiff libjpeg webp little-cms2
+            $ pip3 install newspaper3k
+            $ curl https://raw.githubusercontent.com/codelucas/newspaper/master/download_corpora.py | python3
+
+'''
+
+# Error when running script:
+#   Can run the script from PyCharm Python Console, but fails when running script via
+#       Pycharm 'run' command or terminal.
+from newspaper import Article
+
+url = "http://opensourceforu.com/2016/02/ionic-a-ui-framework-to-simplify-hybrid-mobile-app-development/"
+
+article = Article(url)
+
+#1. Download the article
+article.download()
+
+#2. Parse the article
+article.parse()
+
+#3. Fetch Author Names
+print(article.authors)
+
+#4 Fetch Publication Date
+print("Article Publication Date:")
+print(article.publish_date)
+#5. The URL of the Major Image
+print("Major Image in the article:")
+print(article.top_image)
+
+#6. Natural Language Processing on Article to fetch keywords
+article.nlp()
+print("Keywords in the article")
+print(article.keywords)
+
+#7. Generate article summary
+print("Article Summary:")
+print(article.summary)
