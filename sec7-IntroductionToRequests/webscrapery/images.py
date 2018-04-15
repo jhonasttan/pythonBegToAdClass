3b54bd84ba4433265f352dd3235d0bfdae74b4f3
jhonasttan <jhonasttan@gmail.com>
2018-04-14 21:59:26 -0400

    Sect: 8 - Project #3 - Web Scraper - 66. Improvements to our web scraper
    Signed-off-by: jhonasttan <jhonasttan@gmail.com>

--- sec7-IntroductionToRequests/webscrapery/images.py
+++ sec7-IntroductionToRequests/webscrapery/images.py
@@ -2,18 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 from PIL import Image
 from io import BytesIO
+import os
 
-url = "http://www.bing.com/images/search"
-search = input("Search for: ")
-params = {"q": search}
-r = requests.get(url, params=params)
 
-soup = BeautifulSoup(r.text, "html.parser")
-links = soup.findAll("a", {"class": "thumb"})
 
-for items in links:
-    img_obj = requests.get(items.attrs["href"])
-    print("Getting - ", items.attrs["href"])
-    title = items.attrs["href"].split("/")[-1]
-    img = Image.open(BytesIO(img_obj.content))
-    img.save("./scraped_images/" + title, img.format)
+def StartSearch():
+    url = "http://www.bing.com/images/search"
+    search = input("Search for: ")
+    params = {"q": search}
+    dir_name = search.replace(" ", "_").lower()
+
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+
+    r = requests.get(url, params=params)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.findAll("a", {"class": "thumb"})
+
+    for items in links:
+        img_obj = requests.get(items.attrs["href"])
+        print("Getting - ", items.attrs["href"])
+        title = items.attrs["href"].split("/")[-1]
+        try:
+            img = Image.open(BytesIO(img_obj.content))
+            img.save("./" + dir_name + "/" + title, img.format)
+        except:
+            print("Could not save image")
+    StartSearch()
+
+StartSearch()
