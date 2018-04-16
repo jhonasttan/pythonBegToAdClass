f76ff18b08e2b208bc2bb4059981d2f5748df454
jhonasttan <jhonasttan@gmail.com>
2018-04-16 19:30:24 -0400

    Sect: 10 - Project #5 - Web Development Project Using Web.py - 75. HTML templates
    Signed-off-by: jhonasttan <jhonasttan@gmail.com>

--- sect10-project5-WebDevelopmentProjectUsingWebPy/main.py
+++ sect10-project5-WebDevelopmentProjectUsingWebPy/main.py
@@ -1,15 +1,15 @@
 import web
 
 urls = (
-    '/(.*)', 'index'
+    '/(.*)/(.*)', 'index'
 )
 
-
+render = web.template.render("resources/")
 app = web.application(urls, globals())
 
 class index:
-    def GET(self, name):
-        return '<h1>Hello ' + name + '.</h1> How are you today?'
+    def GET(self, name, age):
+        return render.main(name, age)
 
 
 if __name__ == "__main__":
